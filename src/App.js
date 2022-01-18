84408751f64e700ab58827e5f0ba68e83950d496
Oscar Voit <oscarvoit@gmail.com>
2022-01-18 11:12:13 -0300

    adicionando drawer

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ import Home from './pages/Home'
 
 const App = () => {
  return (
-   <TemplateDefault>
-      <Router>
+   <Router>
+      <TemplateDefault>
         <Switch>
           <Route path="/customers">
             <TemplatePage title="Clientes" Component={Customers} />
@@ -22,8 +22,8 @@ const App = () => {
             <TemplatePage title="Home" Component={Home} />
           </Route>
         </Switch>
-      </Router>
-    </TemplateDefault>
+      </TemplateDefault>
+    </Router>
   )
 }
 
--- src/partials/Header/Header.js
+++ src/partials/Header/Header.js
@@ -1,30 +1,71 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
 import { 
   AppBar, 
   Toolbar,
   Typography,
   Button,
   IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from '@material-ui/core'
 
 import MenuIcon from '@material-ui/icons/Menu'
+import HomeIcon from '@material-ui/icons/Home'
+import PersonAddIcon from '@material-ui/icons/PersonAdd'
 
 import useStyles from './Header.style'
 
 const Header = () => {
   const classes = useStyles()
+  
+  const history = useHistory()
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  const handleMenuClick = route => {
+    history.push(route)
+    handleToggleMenu()
+  }
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" aria-label="menu" color="secondary">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Voit App
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" aria-label="menu" color="secondary" onClick={() => handleToggleMenu()}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Voit App
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={menuOpen} onClose={() => handleToggleMenu()}>
+        <List>
+          <ListItem button onClick={() => handleMenuClick('/')}>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText>Home</ListItemText>
+          </ListItem>
+          <ListItem button onClick={() => handleMenuClick('/customers')}>
+            <ListItemIcon>
+              <PersonAddIcon />
+            </ListItemIcon>
+            <ListItemText>Cadastro de Cliente</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
 
   )
 }
