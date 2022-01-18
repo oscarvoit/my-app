4bc544e310a6c1adcd102778424cd09492a77394
Oscar Voit <oscarvoit@gmail.com>
2022-01-18 18:48:52 -0300

    adicionando snackbar no page register

--- src/pages/customers/Register.js
+++ src/pages/customers/Register.js
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import axios from 'axios'
 import { makeStyles } from '@material-ui/core/styles'
 
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+import { TextField, Button } from '@material-ui/core'
+
+import Toasty from '../../components/Toasty'
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
@@ -25,6 +26,9 @@ const Register = () => {
     },   
   })
 
+  const [openToasty, setOpenToasty] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
   const handleInputChange = (e) => {
     const { name, value } = e.target
 
@@ -37,6 +41,8 @@ const Register = () => {
   }
 
   const handleRegisterButton = () => {
+    setIsLoading(true)
+
     let hasError = false
     let newFormState = {
       ...form,
@@ -67,8 +73,9 @@ const Register = () => {
     axios.post('https://reqres.in/api/users', {
       name: form.name.value,
       job: form.job.value,
-    }).then(response => {
-        console.log('ok', response)
+    }).then((response) => {
+        setOpenToasty(true)
+        setIsLoading(false)
       })
   }
 
@@ -97,8 +104,18 @@ const Register = () => {
         onChange={handleInputChange} />
       </div>
       <div className={classes.wrapper}>
-        <Button variant="contained" color="primary" onClick={handleRegisterButton}>Cadastrar</Button>
+        <Button variant="contained" color="primary" onClick={handleRegisterButton}>
+          {
+            isLoading ? 'Aguarde ...' :  'Cadastrar'
+          }
+        </Button>
       </div>
+      <Toasty
+        open={openToasty}
+        severity="success"
+        text="Cadastro realizado com sucesso!"
+        onClose={() => setOpenToasty(false)}
+      />
     </>
   )
 }
